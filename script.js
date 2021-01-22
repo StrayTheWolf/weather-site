5397079f58b7c83c11b8cc1871f894d8d2967cee
Wolf Stanislav <ork2004@yandex.ru>
2021-01-22 16:32:26 +0300

    is working

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
-document.addEventListener('DOMContentLoaded',() =>{
+document.addEventListener('DOMContentLoaded', () => {
     fetch('http://api.weatherstack.com/current?access_key=3f38b6d0ea2f177df2664e21d8c3b9e0&query=Rostov-on-don')
         .then((response) => {
             return response.json();
         })
         .then((data) => {
-            data.forEach((item) => {
-                let html =  <div className="card text-center">
-                               <div className="card-body">
-                                   <h5 className="card-title">${item.location.name}</h5>
-                                      <p className="card-text">${item.current.observation_time}</p>
-                                      <div className="card-footer text-muted">${data.current.temperature}</div>
-                                      <img src="" alt=""/>
-                                 </div>
-                           </div>;
-
-                document.getElementById('weather-content').insertAdjacentHTML("beforeend", html);
-            });
-
-
-
-
-            console.log(data.location.name);
-            console.log(data.current.observation_time);
-            console.log(data.current.temperature);
-        });
+            let html = `<div class="card text-center">
+                    <div class="card-body">
+                        <h5 class="card-title">город: ${data.location.name}</h5>
+                        <p class="card-text">время ${data.current.observation_time}</p>
+                        <div class="card-footer text-muted">температура: ${data.current.temperature}</div>
+                        <div class="weather-icon"><img src="${data.current.weather_icons}" alt=""/></div>
+                    </div>
+                </div>`;
+            document.getElementById('weather-content').insertAdjacentHTML("beforeend", html);
+            console.log(data.weather_icons)
+        })
 })
-
-
